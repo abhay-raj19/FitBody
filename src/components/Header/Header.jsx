4d37d97de43ef82ef7e79d1227107f62f5587cfd
ani-netizen <guptaanirudh179@gmail.com>
2023-10-03 19:57:09 +0530

    made navbar responsive and added effects
    Signed-off-by: ani-netizen <guptaanirudh179@gmail.com>

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,100 +1,105 @@
-import React,{useRef} from 'react'
-import '../../styles/header.css';
-import logo from '../../assets/img/dumble.png'
-import { useEffect } from 'react';
-
-
+import React, { useRef } from "react";
+import "../../styles/header.css";
+import logo from "../../assets/img/dumble.png";
+import { useEffect } from "react";
 
 const nav__links = [
   {
-    path: '#home',
-    display: 'Home'
+    path: "#home",
+    display: "Home",
   },
   {
-    path: '#schedule',
-    display: 'Schedule'
+    path: "#schedule",
+    display: "Schedule",
   },
   {
-    path: '#classes',
-    display: 'Classes'
+    path: "#classes",
+    display: "Classes",
   },
   {
-    path: '#pricing-plan',
-    display: 'Pricing'
+    path: "#pricing-plan",
+    display: "Pricing",
   },
-]
-
+];
 
 const Header = () => {
   const headerRef = useRef(null);
-  const headerFunc = () =>{
+  const headerFunc = () => {
     if (
-      document.body.scrollTop >80 ||
-      document.documentElement.scrollTop >80
+      document.body.scrollTop > 80 ||
+      document.documentElement.scrollTop > 80
     ) {
       headerRef.current.classList.add("sticky_header");
-    } else{
+    } else {
       headerRef.current.classList.remove("sticky_header");
     }
   };
 
   useEffect(() => {
-    window.addEventListener("scroll",headerFunc);
-  
-    return () => window.removeEventListener("scroll",headerFunc);
+    window.addEventListener("scroll", headerFunc);
+
+    return () => window.removeEventListener("scroll", headerFunc);
   }, []);
 
-  const handleClick = e => {
-    e.preventDefault()
+  const handleClick = (e) => {
+    e.preventDefault();
 
-    const targetAttr = e.target.getAttribute('href')
-    const location = document.querySelector(targetAttr).offsetTop
+    const targetAttr = e.target.getAttribute("href");
+    const location = document.querySelector(targetAttr).offsetTop;
 
     window.scrollTo({
-      left:0,
-      top:location - 80,
+      left: 0,
+      top: location - 80,
     });
-
-  }
-  
+  };
 
   return (
-    <header className='header' ref={headerRef}>
+    <header className="header" ref={headerRef}>
       <div className="container">
         <div className="nav__wrapper">
-
           {/*=====LOGO===*/}
 
-          <div className="logo">
-            <div className="logo_img">
-              <a href="/"><img src={logo} alt="logo" /></a>
+          <input type="checkbox" id="nav-check" hidden />
+          <div className="btn__wrapper">
+            <div className="logo">
+              <div className="logo_img">
+                <img src={logo} alt="" />
+              </div>
+              <h2>FitBody</h2>
             </div>
-            <h2 className='fitbody'><a  href="/">FitBody</a></h2>
+
+            <div className="nav-btn">
+              <label htmlFor="nav-check">
+                <span></span>
+                <span></span>
+                <span></span>
+              </label>
+            </div>
           </div>
 
           {/*====Navigation bar====*/}
 
+          <div className="nav__links">
+            <div className="navigation">
+              <ul className="menu">
+                {nav__links.map((item, index) => (
+                  <li className="nav_item" key={index}>
+                    <a onClick={handleClick} href={item.path}>
+                      {item.display}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
 
-          <div className="navigation">
-            <ul className="menu">
-              {
-                nav__links.map(item => (
-                  <li className="nav_item">
-                  <a onClick={handleClick} href={item.path}>{item.display}</a></li>
-                ))
-              }
-            </ul>
-          </div>
+            {/* Nav Right */}
 
-
-          {/* Nav Right */}
-
-
-          <div className="nav_right">
-            <button className="register_btn">Register</button>
-            <span className="mobile__menu">
-              <i class="ri-menu-line"></i>
-            </span>
+            <div className="nav_right">
+              <button className="register_btn">Register</button>
+              {/* <span className="mobile__menu">
+                <i className="ri-menu-line"></i>
+              </span> */}
+            </div>
           </div>
         </div>
       </div>
