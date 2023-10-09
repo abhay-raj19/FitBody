4a2c0ee2d8a372505fc1b53699c590f71a319781
Ayush Gupta <ayushgupta3902@gmail.com>
2023-10-09 12:16:47 +0530

    chore: Update form design
    Signed-off-by: Ayush Gupta <ayushgupta3902@gmail.com>

--- src/components/UI/ContactUs.jsx
+++ src/components/UI/ContactUs.jsx
@@ -3,24 +3,54 @@ import "../../styles/contactUs.css";
 
 const ContactUs = () => {
   return (
-    <section id="contact-us">
-      <form class="form">
-        <h2>CONTACT US</h2>
-        <p type="Name:">
-          <input placeholder="Write your name here.."></input>
-        </p>
-        <p type="Email:">
-          <input placeholder="Let us know how to contact you back.."></input>
-        </p>
-        <p type="Message:">
-          <input placeholder="What would you like to tell us.."></input>
-        </p>
-        <button>Send Message</button>
-        <div>
-          <span class="fa fa-phone"></span> @abhayraj423
-          <span class="fa fa-envelope-o"></span> rajpootabhay423@gmail.com
+    <section id="contact-us" className="form">
+      <div class="background">
+        <div class="container">
+          <div class="screen">
+            <div class="screen-header">
+              <div class="screen-header-left">
+                <div class="screen-header-button close"></div>
+                <div class="screen-header-button maximize"></div>
+                <div class="screen-header-button minimize"></div>
+              </div>
+              <div class="screen-header-right">
+                <div class="screen-header-ellipsis"></div>
+                <div class="screen-header-ellipsis"></div>
+                <div class="screen-header-ellipsis"></div>
+              </div>
+            </div>
+            <div class="screen-body">
+              <div class="screen-body-item left">
+                <div class="app-title">
+                  <span>CONTACT</span>
+                  <span>US</span>
+                </div>
+                <div class="app-contact"></div>
+              </div>
+              <div class="screen-body-item">
+                <div class="app-form">
+                  <div class="app-form-group">
+                    <input
+                      class="app-form-control"
+                      placeholder="NAME"
+                    />
+                  </div>
+                  <div class="app-form-group">
+                    <input class="app-form-control" placeholder="EMAIL" />
+                  </div>
+
+                  <div class="app-form-group message">
+                    <input class="app-form-control" placeholder="MESSAGE" />
+                  </div>
+                  <div class="app-form-group buttons">
+                    <button class="app-form-button">SEND</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </form>
+      </div>
     </section>
   );
 };
