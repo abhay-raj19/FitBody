6a0c45c0e2affe8cb8ea799fa8a5c228ed337267
Divya Kumari <kumaridivya2796@gmail.com>
2023-10-25 12:36:58 +0530

    added back to tp op button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect , useState } from "react";
+import { useEffect, useState } from "react";
 import Aos from "aos";
 import "./App.css";
 import Header from "./components/Header/Header";
@@ -6,32 +6,32 @@ import Exercises from "./components/UI/Exercises";
 import Footer from "./components/UI/Footer";
 import Hero from "./components/UI/Hero";
 import Pricing from "./components/UI/Pricing";
-import Start from "./components/UI/Start"; 
+import Start from "./components/UI/Start";
 import Testimonials from "./components/UI/Testimonials";
 import ContactUs from "./components/UI/ContactUs";
+import BackToTop from "./components/UI/BackToTop";
 
 function App() {
-
   useEffect(() => {
-    Aos.init()
-  }, [])
-  
+    Aos.init();
+  }, []);
+
   const [isDarkMode, setDarkMode] = useState(false);
 
-
-  return ( 
+  return (
     <>
-    <div  className={isDarkMode ? "dark-mode-app" : "light-mode-app"} >
-    <Header isDarkMode={isDarkMode} setDarkMode={setDarkMode}/> 
-    <Hero/>
-    <Exercises/>
-    <Start/>
-    <Pricing/>
-    <Testimonials/>
-    <ContactUs />
-    <Footer/>
-    </div>
-    </>   
+      <div className={isDarkMode ? "dark-mode-app" : "light-mode-app"}>
+        <Header isDarkMode={isDarkMode} setDarkMode={setDarkMode} />
+        <Hero />
+        <Exercises />
+        <Start />
+        <Pricing />
+        <Testimonials />
+        <ContactUs />
+        <Footer />
+        <BackToTop />
+      </div>
+    </>
   );
 }
 
