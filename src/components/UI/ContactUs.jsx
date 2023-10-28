b33ccad4a35ea1f680f72d41b31c13ccac675d59
Aarav 238 <aarav8090shukla@gmail.com>
2023-10-28 13:32:16 +0530

    added form validation

--- src/components/UI/ContactUs.jsx
+++ src/components/UI/ContactUs.jsx
@@ -1,49 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/contactUs.css";
 
 const ContactUs = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    const newErrors = {};
+
+    if (!formData.name.trim()) {
+      newErrors.name = "Name is required";
+    }
+
+    if (!formData.email.trim()) {
+      newErrors.email = "Email is required";
+    } else if (!isValidEmail(formData.email)) {
+      newErrors.email = "Invalid email address";
+    }
+
+    if (!formData.message.trim()) {
+      newErrors.message = "Message is required";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const isValidEmail = (email) => {
+    // Basic email format validation
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(email);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      console.log("Form data:", formData);
+      alert("Thanks for contacting us!")
+    }
+    
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
     <section id="contact-us" className="form">
-      <div class="background">
-        <div class="container">
-          <div class="screen">
-            <div class="screen-header">
-              <div class="screen-header-left">
-                <div class="screen-header-button close"></div>
-                <div class="screen-header-button maximize"></div>
-                <div class="screen-header-button minimize"></div>
+      <div className="background">
+        <div className="container">
+          <div className="screen">
+            <div className="screen-header">
+              <div className="screen-header-left">
+                <div className="screen-header-button close"></div>
+                <div className="screen-header-button maximize"></div>
+                <div className="screen-header-button minimize"></div>
               </div>
-              <div class="screen-header-right">
-                <div class="screen-header-ellipsis"></div>
-                <div class="screen-header-ellipsis"></div>
-                <div class="screen-header-ellipsis"></div>
+              <div className="screen-header-right">
+                <div className="screen-header-ellipsis"></div>
+                <div className="screen-header-ellipsis"></div>
+                <div className="screen-header-ellipsis"></div>
               </div>
             </div>
-            <div class="screen-body">
-              <div class="screen-body-item left">
-                <div class="app-title">
+            <div className="screen-body">
+              <div className="screen-body-item left">
+                <div className="app-title">
                   <span>CONTACT</span>
                   <span>US</span>
                 </div>
-                <div class="app-contact"></div>
+                <div className="app-contact"></div>
               </div>
-              <div class="screen-body-item">
-                <div class="app-form">
-                  <div class="app-form-group">
+              <div className="screen-body-item">
+                <div className="app-form">
+                  <div className="app-form-group">
                     <input
-                      class="app-form-control"
+                      className="app-form-control"
                       placeholder="NAME"
+                      name="name"
+                      value={formData.name}
+                      onChange={handleInputChange}
                     />
+                    {errors.name && <div style={{color: "red "}} className="error-message">{errors.name}</div>}
                   </div>
-                  <div class="app-form-group">
-                    <input class="app-form-control" placeholder="EMAIL" />
+                  <div className="app-form-group">
+                    <input
+                      className="app-form-control"
+                      placeholder="EMAIL"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleInputChange}
+                    />
+                    {errors.email && <div  style={{color: "red "}} className="error-message">{errors.email}</div>}
                   </div>
-
-                  <div class="app-form-group message">
-                    <input class="app-form-control" placeholder="MESSAGE" />
+                  <div className="app-form-group message">
+                    <input
+                      className="app-form-control"
+                      placeholder="MESSAGE"
+                      name="message"
+                      value={formData.message}
+                      onChange={handleInputChange}
+                    />
+                    {errors.message && <div style={{color: "red "}}  className="error-message">{errors.message}</div>}
                   </div>
-                  <div class="app-form-group buttons">
-                    <button class="app-form-button">SEND</button>
+                  <div className="app-form-group buttons">
+                    <button className="app-form-button" onClick={handleSubmit}>
+                      SEND
+                    </button>
                   </div>
                 </div>
               </div>
