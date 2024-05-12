c3b4799c84b4611d0e6f9866a2cfe941a6b92107
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-05-12 17:48:16 +0530

    added grid in contact us section

--- src/components/UI/ContactUs.jsx
+++ src/components/UI/ContactUs.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import toast from "react-hot-toast";
 import "../../styles/contactUs.css";
+import Lottie from "lottie-react";
+import ContactAnimation from "../../assets/JSON/contactus.json";
 
 const ContactUs = () => {
   const [formData, setFormData] = useState({
@@ -45,46 +47,47 @@ const ContactUs = () => {
 
   return (
     <section id="contact-us" className="form">
-      <div className="w-full form flex justify-center">
-        <div className="contact-us">
+      <div className="w-full form grid justify-center items-center">
+        <div className="contact-animation">
+          <Lottie animationData={ContactAnimation} />
+        </div>
+        <div className="contact-form-container">
           <h1 className="heading text-center">Contact Us</h1>
           {/* contact-us form */}
-          <div className="">
-            <form onSubmit={handleSubmit} className="contact-form">
-              <div className="">
-                <input
-                  type="text"
-                  placeholder="Name"
-                  name="name"
-                  value={formData.name}
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className="">
-                <input
-                  type="email"
-                  placeholder="Email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className="">
-                <textarea
-                  placeholder="Message"
-                  name="message"
-                  rows={7}
-                  value={formData.message}
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className="text-end">
-                <button className="submit" type="submit">
-                  Send
-                </button>
-              </div>
-            </form>
-          </div>
+          <form onSubmit={handleSubmit} className="contact-form">
+            <div className="input-group">
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="input-group">
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="input-group">
+              <textarea
+                placeholder="Message"
+                name="message"
+                rows={7}
+                value={formData.message}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="text-end">
+              <button className="submit" type="submit">
+                Send
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </section>
@@ -92,72 +95,3 @@ const ContactUs = () => {
 };
 
 export default ContactUs;
-
-/**<section id="contact-us" className="form">
-      <div className="background">
-        <div className="container">
-          <div className="screen">
-            <div className="screen-header">
-              <div className="screen-header-left">
-                <div className="screen-header-button close"></div>
-                <div className="screen-header-button maximize"></div>
-                <div className="screen-header-button minimize"></div>
-              </div>
-              <div className="screen-header-right">
-                <div className="screen-header-ellipsis"></div>
-                <div className="screen-header-ellipsis"></div>
-                <div className="screen-header-ellipsis"></div>
-              </div>
-            </div>
-            <div className="screen-body">
-              <div className="screen-body-item left">
-                <div className="app-title">
-                  <span>CONTACT</span>
-                  <span>US</span>
-                </div>
-                <div className="app-contact"></div>
-              </div>
-              <div className="screen-body-item">
-                <div className="app-form">
-                  <div className="app-form-group">
-                    <input
-                      className="app-form-control"
-                      placeholder="NAME"
-                      name="name"
-                      value={formData.name}
-                      onChange={handleInputChange}
-                    />
-                    {errors.name && <div style={{color: "red "}} className="error-message">{errors.name}</div>}
-                  </div>
-                  <div className="app-form-group">
-                    <input
-                      className="app-form-control"
-                      placeholder="EMAIL"
-                      name="email"
-                      value={formData.email}
-                      onChange={handleInputChange}
-                    />
-                    {errors.email && <div  style={{color: "red "}} className="error-message">{errors.email}</div>}
-                  </div>
-                  <div className="app-form-group message">
-                    <input
-                      className="app-form-control"
-                      placeholder="MESSAGE"
-                      name="message"
-                      value={formData.message}
-                      onChange={handleInputChange}
-                    />
-                    {errors.message && <div style={{color: "red "}}  className="error-message">{errors.message}</div>}
-                  </div>
-                  <div className="app-form-group buttons">
-                    <button className="app-form-button" onClick={handleSubmit}>
-                      SEND
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section> */
