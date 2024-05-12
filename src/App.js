cbac7b2cd4b67f2aba1c41e8fbc2a932ee552026
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-05-12 09:29:08 +0530

    added toaster globally

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Start from "./components/UI/Start";
 import Testimonials from "./components/UI/Testimonials";
 import ContactUs from "./components/UI/ContactUs";
 import BackToTop from "./components/UI/BackToTop";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   useEffect(() => {
@@ -30,6 +31,7 @@ function App() {
         <ContactUs />
         <Footer />
         <BackToTop />
+        <Toaster />
       </div>
     </>
   );
