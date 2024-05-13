849edfd9662aefc4c55aad48e0e359d5cad080f1
ujjwalexcodie <kumarujjwalagrawal4@gmail.com>
2024-05-13 20:49:08 +0530

    Added the validation Files to the backend Server Folder with Zod schema passed as middleware

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { registerUser, authUser } from '../controllers/authcontroller.js';
-
+const validate = require("../validation/validate.schema.js")
+const RegisterSchema = require("../validation/zodschema.js")
 const router = express.Router();
 
 // Register the User
-router.post('/register', registerUser);
-router.post('/login',authUser);
+router.post('/register',validate(RegisterSchema), registerUser);
+router.post('/login',validate(RegisterSchema),authUser);
 
 
 
