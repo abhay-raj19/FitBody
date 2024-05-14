6c12924022d0d4c86f7ba0a308d5e389238a2577
Satyam8932 <satyamsingh5august@gmail.com>
2024-05-14 23:19:49 +0530

    deleted
    Signed-off-by: Satyam8932 <satyamsingh5august@gmail.com>

--- server/routes/welcomeRoute.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-var router = Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  return res.send("<h1>Hello Welcome to Fitbody</h1>");
-});
-
-export default router;
