19d5f6f8bc42b2cf445cc958e5c8bff3b1acb24b
Om Kharadkar <omkharadkar664@gmail.com>
2024-05-14 19:11:23 +0530

    Signed-off-by: Om Kharadkar <omkharadkar664@gmail.com> Twitter logo changed

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../../styles/footer.css";
 import logo from "../../assets/img/dumble.png";
 
+
 const Footer = () => {
   const year = new Date().getFullYear();
 
@@ -74,7 +75,9 @@ const Footer = () => {
           </a>
           <a href="/#">
             {" "}
-            <i class="ri-twitter-fill"></i>{" "}
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="25" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+              <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
+            </svg>{" "}
           </a>
 
           {/* <i class="ri-twitter-x-fill"></i> */}
