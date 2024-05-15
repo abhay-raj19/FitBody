4ff62c6ca8170c2bd71a8008cd1ff7fc5c128c04
Mayur <mayursonare18@gmail.com>
2024-05-15 17:28:32 +0530

    Enhancement-#198: Adding blur effect.

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -14,24 +14,24 @@ const Hero = () => {
 
                     <div className="hero_content">
                         <h2
-                         className='section_title'
-                         data-aos='fade-up'
-                         data-aos-duration='1500'
-                         >Exercise is the key to a
+                            className='section_title'
+                            data-aos='fade-up'
+                            data-aos-duration='1500'
+                        >Exercise is the key to a
                             <span className="highlights"> Healthy </span> Lifestyle</h2>
                         <p data-aos='fade-up'
-                        data-aos-delay='100'
-                         data-aos-duration='1800'> Make exercise a daily habit.
+                            data-aos-delay='100'
+                            data-aos-duration='1800'> Make exercise a daily habit.
                             <br />
                             Exercise isn't just the key; it's the driving force  <br />that propels you toward a life of boundless health and vitality
- </p>
+                        </p>
                         <div className="hero_btns" data-aos='fade-up'
-                        data-aos-delay='200'
-                         data-aos-duration='2000'>
+                            data-aos-delay='200'
+                            data-aos-duration='2000'>
                             <button className="register_btn"><Link to="/classes">Get Started</Link></button>
                             <button className="watch_btn">
                                 <span>
-                                    <i class="ri-play-fill"></i>
+                                    <i className="ri-play-fill"></i>
                                 </span>
                                 <span className="watch_video">Watch Video</span>
                             </button>
@@ -52,22 +52,22 @@ const Hero = () => {
                             </div>
 
                             <div className="heart_rate" data-aos='fade-right'
-                         data-aos-duration='1500'>
+                                data-aos-duration='1500'>
                                 <h5>Heart Rate</h5>
-                                <span><i class="ri-heart-pulse-fill"></i></span>
+                                <span><i className="ri-heart-pulse-fill"></i></span>
                                 <br />
                                 <h5>2567 BMP</h5>
                             </div>
 
                             <div className="gym_location" data-aos='fade-left'
-                         data-aos-duration='1500'>
-                                <span><i class="ri-map-pin-2-fill"></i></span>
-                                <h5>Find a new<br/> gym near you</h5>
+                                data-aos-duration='1500'>
+                                <span><i className="ri-map-pin-2-fill"></i></span>
+                                <h5>Find a new<br /> gym near you</h5>
                             </div>
 
 
                             <div className="dumble_icon" data-aos='fade-down'
-                         data-aos-duration='1500'>
+                                data-aos-duration='1500'>
                                 <img src={dumbleIcon} alt="" />
                             </div>
 
