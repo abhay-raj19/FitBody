975cfabb599ca32437890d82fd3356736833009f
ujjwalexcodie <kumarujjwalagrawal4@gmail.com>
2024-05-18 15:42:49 +0530

    All Zod Files Added

--- server/routes/auth-route.js
+++ server/routes/auth-route.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import { registerUser, authUser, changePassword } from '../controllers/auth-controller.js';
-
+import { RegisterSchema, LoginSchema } from '../validation/zodschema.js';
+import { validate } from '../validation/validation.schema.js';
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login',authUser);
+router.post('/register',validate(RegisterSchema), registerUser);
+router.post('/login',validate(LoginSchema) ,authUser);
 router.post('/resetpassword',changePassword)
-
-
-export default router;
