76c71a227391a448bd2fb54b8f1a719ae4970eea
Chetan8299 <csingh8299@gmail.com>
2024-05-29 09:47:04 +0530

    navbar darkmode and z-index issue fix
    Signed-off-by: Chetan8299 <csingh8299@gmail.com>

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,169 +7,170 @@ import { useGSAP } from "@gsap/react";
 gsap.registerPlugin(useGSAP);
 
 const nav__links = [
-	{
-		path: "#home",
-		display: "Home",
-	},
-	{
-		path: "#schedule",
-		display: "Schedule",
-	},
-	{
-		path: "#classes",
-		display: "Classes",
-	},
-	{
-		path: "#pricing-plan",
-		display: "Pricing",
-	},
-	{
-		path: "#contact-us",
-		display: "Contact Us",
-	},
+  {
+    path: "#home",
+    display: "Home",
+  },
+  {
+    path: "#schedule",
+    display: "Schedule",
+  },
+  {
+    path: "#classes",
+    display: "Classes",
+  },
+  {
+    path: "#pricing-plan",
+    display: "Pricing",
+  },
+  {
+    path: "#contact-us",
+    display: "Contact Us",
+  },
 ];
 const Header = ({ isDarkMode, setDarkMode }) => {
-	const [open, setOpen] = useState(false);
-	const [headerBackground, setHeaderBackground] = useState(
-		isDarkMode ? "#111" : "#fff"
-	);
-	const headerRef = useRef(null);
+  const [open, setOpen] = useState(false);
+  const [headerBackground, setHeaderBackground] = useState(
+    isDarkMode ? "#111" : "#fff"
+  );
+  const headerRef = useRef(null);
 
-	const toggleDarkMode = () => {
-		setDarkMode(!isDarkMode);
-	};
+  const toggleDarkMode = () => {
+    setDarkMode(!isDarkMode);
+  };
 
-	const toggleMobileMenu = () => {
-		const navCheck = document.getElementById("nav-check");
-		if (navCheck) {
-			navCheck.checked = !navCheck.checked;
-		}
-	};
+  const toggleMobileMenu = () => {
+    const navCheck = document.getElementById("nav-check");
+    if (navCheck) {
+      navCheck.checked = !navCheck.checked;
+    }
+  };
 
-	const headerFunc = () => {
-		if (
-			document.body.scrollTop > 80 ||
-			document.documentElement.scrollTop > 80
-		) {
-			setHeaderBackground(isDarkMode ? "#111" : "#fff");
-			headerRef.current.classList.add("sticky_header");
-		} else {
-			headerRef.current.classList.remove("sticky_header");
-		}
-	};
+  const headerFunc = () => {
+    if (
+      (document.body.scrollTop > 80 ||
+      document.documentElement.scrollTop > 80)
+    ) {
+      setHeaderBackground(isDarkMode ? "#111" : "#fff");
+    //   headerRef.current.classList.add("sticky_header");
+      headerRef.current.classList.add("fixed_header");
 
-	useEffect(() => {
-		setHeaderBackground(isDarkMode ? "#111" : "#fff");
-		window.addEventListener("scroll", headerFunc);
-		window.scrollTo(window.scrollX, window.scrollY - 1);
-		window.scrollTo(window.scrollX, window.scrollY + 1);
+    } else {
+    //   headerRef.current.classList.remove("sticky_header");
+      headerRef.current.classList.remove("fixed_header");
+    }
+  };
 
-		return () => {
-			window.scrollTo(window.scrollX, window.scrollY - 1);
-			window.scrollTo(window.scrollX, window.scrollY + 1);
-			return window.removeEventListener("scroll", headerFunc);
-		};
-	}, [isDarkMode]);
+  useEffect(() => {
+    setHeaderBackground(isDarkMode ? "#111" : "#fff");
+    window.addEventListener("scroll", headerFunc);
+    window.scrollTo(window.scrollX, window.scrollY - 1);
+    window.scrollTo(window.scrollX, window.scrollY + 1);
 
-	const handleClick = (e) => {
-		e.preventDefault();
+    return () => {
+      window.scrollTo(window.scrollX, window.scrollY - 1);
+      window.scrollTo(window.scrollX, window.scrollY + 1);
+      return window.removeEventListener("scroll", headerFunc);
+    };
+  }, [isDarkMode]);
 
-		const targetAttr = e.target.getAttribute("href");
-		const location = document.querySelector(targetAttr).offsetTop;
+  const handleClick = (e) => {
+    e.preventDefault();
 
-		window.scrollTo({
-			left: 0,
-			top: location - 80,
-		});
-		toggleMobileMenu();
-	};
+    const targetAttr = e.target.getAttribute("href");
+    const location = document.querySelector(targetAttr).offsetTop;
 
-	const headerClass = isDarkMode ? "header dark-mode" : "header";
+    window.scrollTo({
+      left: 0,
+      top: location - 80,
+    });
+    toggleMobileMenu();
+  };
 
-	return (
-		<header
-			className={headerClass}
-			ref={headerRef}
-			style={{
-				background: "rgba(39, 39, 39, 0.1)",
-				borderRadius: "20px",
-				backdropFilter: "blur(60px)",
-				border: "2px solid rgba(255, 255, 255, 0.2)",
-				boxShadow: "0 0 80px rgba(0, 0, 0, 0.3)"
-			}}
-		>
-			<div className="container">
-				<div className="nav__wrapper">
-					{/*=====LOGO===*/}
+  const headerClass = isDarkMode ? "header dark-mode sticky_header" : "header sticky_header";
 
-					<input type="checkbox" id="nav-check" hidden />
-					<div className="btn__wrapper">
-						<div className="logo">
-							<div className="logo_img">
-								<img src={logo} alt="" />
-							</div>
-							<div className="icon_logo">
-								<h2>
-									<a href="$">FitBody</a>
-								</h2>
-							</div>
-							<div
-								className="styles-container"
-								onClick={toggleDarkMode}
-							>
-								<div className="styles-icon">🌙</div>
-								<div className="styles-icon">🔆</div>
-								<div
-									className="styles-ball"
-									style={
-										!isDarkMode
-											? { transform: 'translateX(0%)' }
-											: { transform: 'translateX(140%)' }
-									}
-								/>
-							</div>
-						</div>
+  return (
+    <header
+      className={headerClass}
+      ref={headerRef}
+      style={{
+        background: "rgba(39, 39, 39, 0.1)",
+        borderRadius: "20px",
+        backdropFilter: "blur(60px)",
+        border: "2px solid rgba(255, 255, 255, 0.2)",
+        boxShadow: "0 0 80px rgba(0, 0, 0, 0.3)",
+      }}
+    >
+      <div className="container">
+        <div className="nav__wrapper">
+          {/*=====LOGO===*/}
 
-						<div className="nav-btn">
-							<label htmlFor="nav-check">
-								<span></span>
-								<span></span>
-								<span></span>
-							</label>
-						</div>
-					</div>
+          <input type="checkbox" id="nav-check" hidden />
+          <div className="btn__wrapper">
+            <div className="logo">
+              <div className="logo_img">
+                <img src={logo} alt="" />
+              </div>
+              <div className="icon_logo">
+                <h2>
+                  <a href="$">FitBody</a>
+                </h2>
+              </div>
+              <div className="styles-container" onClick={toggleDarkMode}>
+                <div className="styles-icon">🌙</div>
+                <div className="styles-icon">🔆</div>
+                <div
+                  className="styles-ball"
+                  style={
+                    !isDarkMode
+                      ? { transform: "translateX(0%)" }
+                      : { transform: "translateX(140%)" }
+                  }
+                />
+              </div>
+            </div>
 
-					{/*====Navigation bar====*/}
+            <div className="nav-btn">
+              <label htmlFor="nav-check">
+                <span></span>
+                <span></span>
+                <span></span>
+              </label>
+            </div>
+          </div>
 
-					<div className="nav__links">
-						<div className="navigation">
-							<ul className="menu">
-								{nav__links.map((item, index) => (
-									<li className="nav_item" key={index}>
-										<a
-											onClick={handleClick}
-											href={item.path}
-										>
-											{item.display}
-										</a>
-									</li>
-								))}
-							</ul>
-						</div>
+          {/*====Navigation bar====*/}
 
-						{/* Nav Right */}
+          <div
+            className={`${
+              isDarkMode ? "nav__links dark_nav__links" : "nav__links"
+            }`}
+          >
+            <div className="navigation">
+              <ul className="menu">
+                {nav__links.map((item, index) => (
+                  <li className="nav_item" key={index}>
+                    <a onClick={handleClick} href={item.path}>
+                      {item.display}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
 
-						<div className="nav_right">
-							<button className="register_btn">Register</button>
-							{/* <span className="mobile__menu">
+            {/* Nav Right */}
+
+            <div className="nav_right">
+              <button className="register_btn">Register</button>
+              {/* <span className="mobile__menu">
                 <i className="ri-menu-line"></i>
               </span> */}
-						</div>
-					</div>
-				</div>
-			</div>
-		</header>
-	);
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
